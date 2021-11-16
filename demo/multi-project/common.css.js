e4d95389eef40680a1992e78be97801126025df8
xiyu97 <1010857039@qq.com>
2021-11-16 11:09:08 +0800

    ..

--- demo/multi-project/common.css.js
+++
@@ -1,9 +0,0 @@
-import './src/style/default.pcie8.css'
-import './src/style/font-awesome.css'
-
-
-// import "core-js/stable";
-// import "regenerator-runtime/runtime";
-
-import './src/js/jquery-1.9.0.min.js'
-import './src/js/app.pcie8'
